974f2f42afe55a75d1d62e7ca79c755e7df00646
iamstan13y <iamstan13y@gmail.com>
2023-04-20 11:11:54 +0200

    Added Account Model.

---
+++ iDent.ModelLibrary/Models/Data/Account.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iDent.ModelLibrary.Models.Data
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        [NotMapped]
+        public string? Token { get; set; }
+    }
+}
