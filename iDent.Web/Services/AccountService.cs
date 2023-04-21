df6d7465f210fc25347f36a72b0b579bd7da91ef
iamstan13y <iamstan13y@gmail.com>
2023-04-21 11:23:31 +0200

    Account Service Configuration.

---
+++ iDent.Web/Services/AccountService.cs
@@ -0,0 +1,49 @@
+﻿using iDent.ModelLibrary.Models.Data;
+using iDent.ModelLibrary.Models.Local;
+using iDent.Web.Services.IServices;
+
+namespace iDent.Web.Services
+{
+    public class AccountService : IAccountService
+    {
+        public Task<Result<Account>> AddAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<bool>> DeleteAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<IEnumerable<Account>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Account>> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<string>> GetResetPasswordCodeAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Account>> LoginAsync(LoginRequest login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<string>> ResendOtpAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Account>> UpdateAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
