78e05edab5b17754a0cbdc3d37c9e07045bc7e2e
iamstan13y <iamstan13y@gmail.com>
2023-04-21 11:04:33 +0200

    Account Repository Pattern & DI.

---
+++ iDent.Web/Models/Repository/IRepository/IAccountRepository.cs
@@ -0,0 +1,20 @@
+﻿using iDent.ModelLibrary.Models.Data;
+using iDent.ModelLibrary.Models.Local;
+
+namespace iDent.Web.Models.Repository.IRepository
+{
+    public interface IAccountRepository
+    {
+        Task<Result<Account>> AddAsync(Account account);
+        Task<Result<Account>> GetByIdAsync(int id);
+        Task<Result<IEnumerable<Account>>> GetAllAsync();
+        Task<Result<Account>> UpdateAsync(Account account);
+        Task<Result<bool>> DeleteAsync(Account account);
+        Task<Result<Account>> LoginAsync(LoginRequest login);
+        //Task<Result<Account>> ChangePasswordAsync(ChangePasswordRequest changePassword);
+        Task<Result<string>> GetResetPasswordCodeAsync(string email);
+       // Task<Result<Account>> ResetPasswordAsync(ResetPasswordRequest resetPassword);
+        //Task<Result<Account>> ConfirmAccountAsync(ConfirmAccountRequest confirmAccount);
+        Task<Result<string>> ResendOtpAsync(string email);
+    }
+}
