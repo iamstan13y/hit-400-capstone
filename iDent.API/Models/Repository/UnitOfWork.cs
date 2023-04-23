c468e76e06905a0bc6c634cc7d4d1c890ccb1d08
iamstan13y <iamstan13y@gmail.com>
2023-04-24 00:40:19 +0200

    Configured Unit Of Work & DI.

---
+++ iDent.API/Models/Repository/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using iDent.API.Models.Data;
+using iDent.API.Models.Repository.IRepository;
+
+namespace iDent.API.Models.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void SaveChanges() => _context.SaveChanges();
+    }
+}
