b751913dec2bf5ec0f0b0c2c6b4892793e611f57
iamstan13y <iamstan13y@gmail.com>
2023-05-04 03:40:29 +0200

    Login Implementation.

--- iDent.Web/Controllers/HomeController.cs
+++ iDent.Web/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace iDent.Web.Controllers
             _accountService = accountService;
         }
 
-        [Authorize]
+        //[Authorize]
         public IActionResult Index()
         {
             return View();
@@ -52,14 +52,14 @@ namespace iDent.Web.Controllers
         {
             var result = await _accountService.LoginAsync(request);
 
-            if (result.Data!.Token == null) return View();
+            if (result.Data?.Token == null) return View();
             
-            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
-            identity.AddClaim(new Claim(ClaimTypes.Email, result.Data!.Email!));
-            var principal = new ClaimsPrincipal(identity);
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+            //var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            //identity.AddClaim(new Claim(ClaimTypes.Email, result.Data!.Email!));
+            //var principal = new ClaimsPrincipal(identity);
+            //await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
 
-            HttpContext.Session.SetString("JWToken", result.Data!.Token);
+            //HttpContext.Session.SetString("JWToken", result.Data!.Token);
             TempData["alert"] = $"Welcome, {result.Data!.Email}!";
 
             return RedirectToAction("Index");
