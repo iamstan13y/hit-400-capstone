a809d2d6c9813521445f505bdc6b471a39721431
iamstan13y <iamstan13y@gmail.com>
2023-05-05 23:35:46 +0200

    WIP.

---
+++ iDent.App/Imports.cs
@@ -0,0 +1,12 @@
+// .NET MAUI Toolkit
+global using CommunityToolkit.Maui;
+// .NET MAUI Markup
+global using CommunityToolkit.Maui.Markup;
+// MVVM Toolkit
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using iDent.App.ViewModels;
+global using iDent.App.Views;
+// Static
+global using static CommunityToolkit.Maui.Markup.GridRowsColumns;
+global using static Microsoft.Maui.Graphics.Colors;
