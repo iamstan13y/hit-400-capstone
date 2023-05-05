4f6b455fe4e1521ed441fd8911c157d191b6ebe8
iamstan13y <iamstan13y@gmail.com>
2023-05-05 23:44:39 +0200

    WIP.

--- iDent.ModelLibrary/Models/Data/Identity.cs
+++ iDent.ModelLibrary/Models/Data/Identity.cs
@@ -2,12 +2,14 @@
 {
     public class Identity
     {
-        public int Id { get; set; }
+        public string? IdentityNumber { get; set; }
         public string? FirstNames { get; set; }
         public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? MobileNumber { get; set; }
         public DateTime DOB { get; set; }
         public string? Address { get; set; }
-        public string? IdentityNumber { get; set; }
+        public int Status { get; set; } = 1;
         public DateTime DateCreated { get; set; } = DateTime.Now;
     }
 }
