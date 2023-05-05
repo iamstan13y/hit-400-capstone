162699870713de766659783fabf25673f2c28405
iamstan13y <iamstan13y@gmail.com>
2023-05-05 23:35:18 +0200

    WIP.

---
+++ iDent.App/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace iDent.App
+{
+	public partial class App : Application
+	{
+		public App(IServiceProvider services)
+		{
+			InitializeComponent();
+
+			MainPage = services.GetService<MainPage>();
+		}
+
+		protected override Window CreateWindow(IActivationState? activationState)
+		{
+			var window = base.CreateWindow(activationState);
+			window.Title = "iDent.App";
+			return window;
+		}
+	}
+}
