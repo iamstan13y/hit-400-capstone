4de4d93192e65e76fcbe26e8066d588b0ec5fb67
iamstan13y <iamstan13y@gmail.com>
2023-05-05 23:44:44 +0200

    WIP.

---
+++ iDent.ModelLibrary/Models/Local/IdentityRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace iDent.ModelLibrary.Models.Local
+{
+    public class IdentityRequest
+    {
+        public string? IdentityNumber { get; set; }
+        public string? FirstNames { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? MobileNumber { get; set; }   
+        public DateTime DOB { get; set; }
+        public string? Address { get; set; }
+    }
+}
