601265f54d6bc844865188e69f68f1a047ff4653
iamstan13y <iamstan13y@gmail.com>
2023-05-05 23:34:00 +0200

    WIP.

---
+++ iDent.App/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace iDent.App.Views
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage(MainViewModel viewModel)
+		{
+			InitializeComponent();
+			BindingContext = viewModel;
+		}
+	}
+}
