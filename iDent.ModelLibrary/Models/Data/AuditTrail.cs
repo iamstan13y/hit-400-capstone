73ba3a3850841b3773a1c5e1529fe598b280c97b
iamstan13y <iamstan13y@gmail.com>
2023-05-05 23:44:29 +0200

    WIP.

---
+++ iDent.ModelLibrary/Models/Data/AuditTrail.cs
@@ -0,0 +1,11 @@
+﻿namespace iDent.ModelLibrary.Models.Data
+{
+    public class AuditTrail
+    {
+        public int Id { get; set; }
+        public int IdentityId { get; set; }
+        public string? Operation { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public Identity? Identity { get; set; }
+    }
+}
