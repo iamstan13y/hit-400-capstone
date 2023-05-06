8ce8076de5c24ee39cdaf532116daab0e77aa3f3
iamstan13y <iamstan13y@gmail.com>
2023-05-06 11:20:13 +0200

    Added Identitty Controller.

---
+++ iDent.API/Controllers/IdentityController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iDent.API.Controllers
+{
+	[Route("api/v1/[controller]")]
+	[ApiController]
+	public class IdentityController : ControllerBase
+	{
+	}
+}
