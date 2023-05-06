bb11b888b7f00c312c0da50d1fc29a6d68776def
iamstan13y <iamstan13y@gmail.com>
2023-05-06 11:16:40 +0200

    Identity Repository Pattern & UoW DI.

---
+++ iDent.API/Models/Repository/IdentityRepository.cs
@@ -0,0 +1,13 @@
+﻿using iDent.API.Models.Data;
+using iDent.API.Models.Repository.IRepository;
+using iDent.ModelLibrary.Models.Data;
+
+namespace iDent.API.Models.Repository
+{
+	public class IdentityRepository : Repository<Identity>, IIdentityRepository
+	{
+		public IdentityRepository(AppDbContext context) : base(context)
+		{
+		}
+	}
+}
