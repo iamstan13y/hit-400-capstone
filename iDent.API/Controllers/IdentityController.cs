065b11ef86d064d5be612310e7bdd8aab695720e
iamstan13y <iamstan13y@gmail.com>
2023-05-07 13:17:54 +0200

    Set Up Profile - I.

--- iDent.API/Controllers/IdentityController.cs
+++ iDent.API/Controllers/IdentityController.cs
@@ -1,4 +1,6 @@
 ﻿using iDent.API.Models.Repository.IRepository;
+using iDent.ModelLibrary.Models.Data;
+using iDent.ModelLibrary.Models.Local;
 using Microsoft.AspNetCore.Mvc;
 
 namespace iDent.API.Controllers
@@ -12,6 +14,31 @@ namespace iDent.API.Controllers
 		public IdentityController(IUnitOfWork unitOfWork)
 		{
 			_unitOfWork = unitOfWork;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Post(IdentityRequest request)
+		{
+			var result = await _unitOfWork.Identity.AddAsync(new Identity
+			{
+				Address = request.Address,
+				DOB = request.DOB,
+				FirstNames = request.FirstNames,
+				Email = request.Email,
+				IdentityNumber = request.IdentityNumber,
+				LastName = request.LastName,
+				MobileNumber = request.MobileNumber
+			});
+
+			if (!result.Success) return BadRequest(result);
+			
+			return Ok(result);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> Get(int id)
+		{
+			var result = await _unitOfWork.Identity.FindAsync()
 		}
 	}
 }
