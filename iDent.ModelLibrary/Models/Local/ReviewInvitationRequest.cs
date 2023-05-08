3ada4c796c3de9e638770256b360b4e4a5756cc8
iamstan13y <iamstan13y@gmail.com>
2023-05-08 11:18:43 +0200

    More Updates & Stuff.

---
+++ iDent.ModelLibrary/Models/Local/ReviewInvitationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace iDent.ModelLibrary.Models.Local
+{
+    public class ReviewInvitationRequest
+    {
+        public int InvitationId { get; set; }
+        public int Status { get; set; }
+    }
+}
