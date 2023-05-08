e9c75e83e6563adfca0783f8d074d1aad36d4f6f
iamstan13y <iamstan13y@gmail.com>
2023-05-08 10:42:40 +0200

    Authenticate Mobile User.

--- iDent.API/Models/Repository/IdentityRepository.cs
+++ iDent.API/Models/Repository/IdentityRepository.cs
@@ -1,6 +1,8 @@
 ﻿using iDent.API.Models.Data;
 using iDent.API.Models.Repository.IRepository;
 using iDent.ModelLibrary.Models.Data;
+using iDent.ModelLibrary.Models.Local;
+using Microsoft.EntityFrameworkCore;
 
 namespace iDent.API.Models.Repository
 {
@@ -9,5 +11,13 @@ namespace iDent.API.Models.Repository
 		public IdentityRepository(AppDbContext context) : base(context)
 		{
 		}
-	}
+
+        public async Task<Result<Identity>> AuthenticateAsync(LoginRequest request)
+        {
+			var identity = await _dbSet.Where(x => x.Email == request.Email).FirstOrDefaultAsync();
+			if (identity == null) return new Result<Identity>(false, "Username or Password is incorrect.");
+
+			return new Result<Identity>(identity);
+        }
+    }
 }
