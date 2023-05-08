5fe95596d5db6c2fc48d8c21e779280adf4b3f8a
iamstan13y <iamstan13y@gmail.com>
2023-05-08 10:42:35 +0200

    Authenticate Mobile User.

--- iDent.API/Models/Repository/IRepository/IIdentityRepository.cs
+++ iDent.API/Models/Repository/IRepository/IIdentityRepository.cs
@@ -1,8 +1,10 @@
 ﻿using iDent.ModelLibrary.Models.Data;
+using iDent.ModelLibrary.Models.Local;
 
 namespace iDent.API.Models.Repository.IRepository
 {
 	public interface IIdentityRepository : IRepository<Identity>
 	{
+		Task<Result<Identity>> AuthenticateAsync(LoginRequest request);
 	}
 }
