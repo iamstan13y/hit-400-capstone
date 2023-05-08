b8465d8ded87d1a61fcf1f67f02bf28191ee2043
iamstan13y <iamstan13y@gmail.com>
2023-05-08 11:18:38 +0200

    More Updates & Stuff.

---
+++ iDent.ModelLibrary/Models/Local/InvitationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace iDent.ModelLibrary.Models.Local
+{
+    public class InvitationRequest
+    {
+        public int IdentityId { get; set; }
+        public int BankId { get; set; }
+        public string? Reason { get; set; }
+    }
+}
