2fd578986a3c2b9b842cc3adfd51fc11c69a4c9b
iamstan13y <iamstan13y@gmail.com>
2023-05-08 16:15:11 +0200

    Invitation Service - WIP.

---
+++ iDent.Web/Services/IServices/IInvitationService.cs
@@ -0,0 +1,14 @@
+﻿using iDent.ModelLibrary.Models.Data;
+using iDent.ModelLibrary.Models.Local;
+
+namespace iDent.Web.Services.IServices
+{
+    public interface IInvitationService
+    {
+        Task<Result<Invitation>> AddAsync(InvitationRequest request);
+        Task<Result<IEnumerable<Invitation>>> GetByBankIdAsync(int bankId);
+        //Task<Result<IEnumerable<Account>>> GetAllAsync();
+        //Task<Result<Account>> UpdateAsync(Account account);
+        //Task<Result<bool>> DeleteAsync(Account account);
+    }
+}
