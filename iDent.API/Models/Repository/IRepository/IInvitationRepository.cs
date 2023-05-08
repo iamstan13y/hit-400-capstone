c6e3cae2e9eca2840816e6c5dfcf3f3df95cda2d
iamstan13y <iamstan13y@gmail.com>
2023-05-08 10:27:10 +0200

    Random Stuff.

--- iDent.API/Models/Repository/IRepository/IInvitationRepository.cs
+++ iDent.API/Models/Repository/IRepository/IInvitationRepository.cs
@@ -1,8 +1,11 @@
 ﻿using iDent.ModelLibrary.Models.Data;
+using iDent.ModelLibrary.Models.Local;
 
 namespace iDent.API.Models.Repository.IRepository
 {
     public interface IInvitationRepository : IRepository<Invitation>
     {
+        Task<Result<IEnumerable<Invitation>>> GetByIdentityIdAsync(int identityId);
+        Task<Result<IEnumerable<Invitation>>> GetByBankIdAsync(int bankId);
     }
 }
