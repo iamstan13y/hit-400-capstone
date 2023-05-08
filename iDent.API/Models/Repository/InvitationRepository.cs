e075d3114f491ac3db21cc8d9576cf06fc47a21a
iamstan13y <iamstan13y@gmail.com>
2023-05-08 09:45:27 +0200

    Updates.

---
+++ iDent.API/Models/Repository/InvitationRepository.cs
@@ -0,0 +1,30 @@
+﻿using iDent.API.Models.Data;
+using iDent.API.Models.Repository.IRepository;
+using iDent.ModelLibrary.Models.Data;
+using iDent.ModelLibrary.Models.Local;
+using iDent.ModelLibrary.Utility;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace iDent.API.Models.Repository
+{
+    public class InvitationRepository : Repository<Invitation>, IInvitationRepository
+    {
+        public InvitationRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async new Task<Result<Invitation>> FindAsync(int identityId)
+        {
+            var invitation = await _dbSet
+                .Where(x => x.IdentityId == identityId)
+                .Include(x => x.Bank)
+                .Include(x => x.Identity)
+                .FirstOrDefaultAsync();
+
+            if (invitation == null) return new Result<Invitation>(false, "Invitation not found.");
+
+            return new Result<Invitation>(invitation);
+        }
+    }
+}
