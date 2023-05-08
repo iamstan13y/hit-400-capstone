9d108d5dc603520d7d22ba0fa1db2cd0cc62dc26
iamstan13y <iamstan13y@gmail.com>
2023-05-08 10:53:30 +0200

    Update Fix.

--- iDent.API/Controllers/IdentityController.cs
+++ iDent.API/Controllers/IdentityController.cs
@@ -5,40 +5,42 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace iDent.API.Controllers
 {
-	[Route("api/v1/[controller]")]
-	[ApiController]
-	public class IdentityController : ControllerBase
-	{
-		private readonly IUnitOfWork _unitOfWork;
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
 
-		public IdentityController(IUnitOfWork unitOfWork)
-		{
-			_unitOfWork = unitOfWork;
-		}
+        public IdentityController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Post(IdentityRequest request)
-		{
-			var result = await _unitOfWork.Identity.AddAsync(new Identity
-			{
-				Address = request.Address,
-				DOB = request.DOB,
-				FirstNames = request.FirstNames,
-				Email = request.Email,
-				IdentityNumber = request.IdentityNumber,
-				LastName = request.LastName,
-				MobileNumber = request.MobileNumber
-			});
+        [HttpPost]
+        public async Task<IActionResult> Post(IdentityRequest request)
+        {
+            var result = await _unitOfWork.Identity.AddAsync(new Identity
+            {
+                Address = request.Address,
+                DOB = request.DOB,
+                FirstNames = request.FirstNames,
+                Email = request.Email,
+                IdentityNumber = request.IdentityNumber,
+                LastName = request.LastName,
+                MobileNumber = request.MobileNumber
+            });
+            
+            _unitOfWork.SaveChanges();
 
-			if (!result.Success) return BadRequest(result);
-			
-			return Ok(result);
-		}
+            if (!result.Success) return BadRequest(result);
 
-		//[HttpGet("{id}")]
-		//public async Task<IActionResult> Get(int id)
-		//{
-		//	var result = await _unitOfWork.Identity.FindAsync()
-		//}
-	}
+            return Ok(result);
+        }
+
+        //[HttpGet("{id}")]
+        //public async Task<IActionResult> Get(int id)
+        //{
+        //	var result = await _unitOfWork.Identity.FindAsync()
+        //}
+    }
 }
